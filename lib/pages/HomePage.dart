8d72aa5473d0d53a7b7f930f06eb26b2cd2395e7
Vichu Bhai <vishnuvarthan@Vishnus-MacBook-Air.local>
2019-05-22 09:02:16 +0530

    Changed A Little in HomePage

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,16 +7,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var key=GlobalKey<FormState>();
+ 
+  var currency ={'JPY':' ¥ ','INR': '₹','Yuan':'¥','EU':'€'};
+  //Controllers
+  var _currencyController =new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
         
-        title: Text("Credito"),
+        title: Text("Credito",style: TextStyle(fontFamily: "Opensans"),),
         centerTitle: true,
     
       ),
+    floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.red,
+      foregroundColor: Colors.black,
+      child: Icon(Icons.add),
+      onPressed: (){
+        setState(() {
+          showDialog(
+            context: context,
+            barrierDismissible: true,
+            child: Form(
+              
+              key: key,
+              child:ListView(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(top: 6.0,left: 6.0,right: 6.0),
+                    margin: EdgeInsets.only(top:3.0),
+                    child: TextFormField(
+                      controller: _currencyController,
+                      maxLength: 16,
+                      decoration: InputDecoration(
+                        icon:Icon(Icons.payment),
+                        hintText: "Card Number",
+                        hintStyle: TextStyle(fontFamily:"OpenSans" )
+                        
+                      ),
+                      style: TextStyle(fontFamily: "OpenSans"),
+
+
+                    ),
+                  )
+                  
+                ],
+              ) 
+            )
+
+          
+            
+
+          );
+         
+          
+        });
+
+      }
+    ),
       
       
     );
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'HomePage.dart';
